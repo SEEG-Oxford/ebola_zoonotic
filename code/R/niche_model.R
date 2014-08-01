2ebab819a5513bf918fb477585f61e9f0694e3ff
Nick <goldingn35@gmail.com>
2014-08-01 16:24:00 +0100

    removed marburg data, ran on many cores and outputted results

--- code/R/niche_model.R
+++ code/R/niche_model.R
@@ -24,6 +24,9 @@ pop <- covs[[which(names(covs) == "afripop_._asiapop_5km")]]
 # occurrence data
 occ <- read.csv('data/occurrence/clean/occurrence.csv')
 
+# remove Marburg data
+occ <- occ[occ$virus != 'Marburg', ]
+
 # generate pseudo-absence data according to the population surface
 # (assume perfect detection of human cases, so reported risk only
 # a function of population density)
@@ -45,13 +48,14 @@ dat_covs <- extract(covs, dat[, -1])
 
 dat_all <- cbind(dat, dat_covs)
 
-ncpu <- 8
+ncpu <- 60
 nboot <- ncpu * 1
 
 # get random bootstraps of the data (minimum 5 pres/5 abs)
 data_list <- replicate(nboot,
                        subsample(dat_all,
-                                 nrow(dat_all)),
+                                 nrow(dat_all),
+                                 minimum = c(10, 10)),
                        simplify = FALSE)
 
 # initialize the cluster
@@ -64,10 +68,41 @@ model_list <- sfLapply(data_list,
                        gbm.y = 1,
                        pred.raster = covs,
                        gbm.coords = 2:3,
-                       wt = function(PA) ifelse(PA == 1, 1, sum(PA) / sum(1 - PA)),
-                       method = 'perf')
+                       wt = function(PA) ifelse(PA == 1, 1, sum(PA) / sum(1 - PA)))
 
+# stop the cluster
 sfStop()
+
+# summarise all the ensembles
+preds <- stack(lapply(model_list, '[[', 4))
+preds_sry <- combinePreds(preds)
+names(preds_sry) <- c('mean',
+                      'median',
+                      'lowerCI',
+                      'upperCI')
+
+# save the prediction summary
+writeRaster(preds_sry,
+            file = 'output/prediction',
+            format = 'GTiff')
+
+# save the relative influence scores
+relinf <- getRelInf(model_list)
+write.csv(relinf,
+          file = 'output/relative_influence.csv',
+          row.names = FALSE)
+
+# plot and the marginal effect curves
+png('output/effects.png',
+    width = 2000,
+    height = 2500,
+    pointsize = 30)
+
+par(mfrow = n2mfrow(nlayers(covs)))
+effects <- getEffectPlots(model_list,
+                          plot = TRUE)
+
+dev.off()
 
 # plot the risk map
 png('output/risk.png',
@@ -78,10 +113,9 @@ png('output/risk.png',
 par(oma = rep(0, 4),
     mar = c(0, 0, 0, 2))
 
-plot(m$pred,
+plot(preds_sry[[1]],
      axes = FALSE,
-     box = FALSE,
-     legend = FALSE)
+     box = FALSE)
 
 points(dat[, -1],
        pch = 16,
