dfc3f1bccbfb77a3b43f2c4ee7e00e6514a4cda7
Nick <goldingn35@gmail.com>
2014-08-03 21:13:36 +0100

    fiddled with bat raster script - weird I/O errors and currently untested

--- code/R/make_bat_rasters.R
+++ code/R/make_bat_rasters.R
@@ -9,12 +9,30 @@ install_github('taxize_',
                'ropensci')
 library(seegSDM)
 library(taxize) # getting genera
-library(rgeos) # gOverlaps
 library(snowfall) # parallel
-library(maptools) # unionSP
 
 # ~~~~~~~~~~
 # define functions 
+
+rasterizeSpecies <- function(species, shape, raster, folder = 'mega_bats') {
+  tmp <- rasterize(shape[shape$BINOMIAL == species, ],
+                   raster,
+                   field = 1,
+                   background = 0,
+                   fun = 'first')
+
+  writeRaster(tmp,
+              filename = paste0('~/tmp/',
+                                folder,
+                                '/',
+                                gsub(' ', '_', species)),
+              format = 'GTiff',
+              overwrite = TRUE)
+  
+  rm(tmp)
+  
+  return (NULL)
+}
 
 # ~~~~~~~~~~
 # load data
@@ -54,25 +72,54 @@ rm(mammterr)
 mega_species <- unique(mega_shp$BINOMIAL)
 micro_species <- unique(micro_shp$BINOMIAL)
 
-sfInit(cpus = 8, parallel = TRUE)
+sfInit(cpus = 60, parallel = TRUE)
 sfLibrary(raster)
 
-rasterize_species <- function(species, shape, raster) {
-  rasterize(shape[shape$BINOMIAL == species, ],
-            raster,
-            field = 1,
-            background = 0,
-            fun = 'first')
+mega_distrib <- sfLapply(mega_species,
+                         rasterizeSpecies,
+                         mega_shp,
+                         template)
+
+micro_distrib <- sfLapply(micro_species,
+                          rasterizeSpecies,
+                          micro_shp,
+                          template,
+                          folder = 'micro_bats')
+
+# loop through these, check if they contain any of the species' range,
+# if they do mask and save them, else delete them
+
+tidySpecies <- function (filename, template) {
+  # load a raster if it containsany of the species' range,
+  # mask and resave it, else delete it
+  tmp <- raster(filename)
+  if (maxValue(tmp) == 1) {
+    tmp <- mask(tmp,
+                template)
+    
+    writeRaster(tmp,
+                file = filename,
+                overwrite = TRUE)
+
+    } else {
+      
+    rm(tmp)
+    file.remove(filename)
+    
+  }
+  
+  return (NULL)
+  
 }
 
-mega_distib <- sfLapply(mega_species,
-                        rasterize_species,
-                        mega_shp,
-                        template)
+mega_files <- list.files('~/tmp/mega_bats/',
+                         full.names = TRUE)
 
-micro_distib <- sfLapply(micro_species,
-                         rasterize_species,
-                         micro_shp,
-                         template)
+micro_files <- list.files('~/tmp/micro_bats/',
+                          full.names = TRUE)
+
+sfLapply(mega_files, tidySpecies, template)
+
+sfLapply(micro_files, tidySpecies, template)
 
 sfStop()
